bf98d398528a7eaa583c6716b76ab380095cbfe4
logonrm <henrique963@hotmail.com>
2017-06-26 22:42:24 -0300

    First commit

--- app/src/main/java/com/example/logonrm/estacoessp/api/RetrofitClient.java
+++ app/src/main/java/com/example/logonrm/estacoessp/api/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.example.logonrm.estacoessp.api;
 
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,7 +15,12 @@ public class RetrofitClient {
 
     public static Retrofit getClient(String baseUrl) {
         if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new StethoInterceptor())
+                    .build();
+
             retrofit = new Retrofit.Builder()
+                    .client(okHttpClient)
                     .baseUrl(baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
